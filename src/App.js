1883222f2ff187a9dea45450d8c294a0a00061ff
Meg <meganriley806@gmail.com>
2022-03-10 19:19:16 -0600

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorefrontIcon from '@material-ui/icons/Storefront';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large"/>
+      </div>
+      <div className="header__search"></div>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span classNmae="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav__item">
+          <span classNmae="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <div className="nav__item">
+          <ShoppingBasketIcon fontSize="large"/>
+          <span className="nav__itemLineTwo">0</span>
+        </div>
+      </div>
     </div>
   );
 }
